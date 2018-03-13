d6ba4e13777889d615a8c3dddb3177ac3227f375
serenaliaojc <36046972+serenaliaojc@users.noreply.github.com>
2018-03-13 16:41:14 -0400

    Create Basic CRUD Views

--- SerenaLiao_MVC/SerenaLiao_MVC/Controllers/SerenaController.cs
+++ SerenaLiao_MVC/SerenaLiao_MVC/Controllers/SerenaController.cs
@@ -25,7 +25,167 @@ namespace SerenaLiao_MVC.Controllers
             {
                 ViewBag.ExceptionMessage = exception.Message;
             }
-            return View();
+
+            return View("~/Views/Errors/Error.cshtml");
+        }
+
+        [HttpGet]
+        public ActionResult Add()
+        {
+            try
+            {
+                return View();
+            }
+            catch (Exception genericException)
+            {
+                ViewBag.ExceptionMessage = genericException.Message;
+            }
+
+            return View("~/Views/Errors/Error.cshtml");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add(Patients patient)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Patients.Add(patient);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+
+                return View(patient);
+            }
+            catch (DbUpdateException DbException) 
+            {
+                ViewBag.DbExceptionMessage = DbException.Message;
+            }
+            catch (Exception genericException)
+            {
+                ViewBag.ExceptionMessage = genericException;
+            }
+
+            return View("~/Views/Errors/Error.cshtml");
+        }
+
+        [HttpGet]
+        public ActionResult Update(int? id)
+        {
+            try
+            {
+                if (id == 0)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                Patients patient = db.Patients.SingleOrDefault(p => p.Patient_Id == id);
+
+                if (patient == null)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                return View(patient);
+            }
+            catch (Exception genericException)
+            {
+                ViewBag.ExceptionMessage = genericException.Message;
+            }
+
+            return View("~/Views/Errors/Error.cshtml");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Update(Patients patient)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Entry(patient).State = System.Data.Entity.EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+
+                return View(patient);
+            }
+            catch (DbUpdateException dbException) 
+            {
+                ViewBag.ErrorMessage = dbException.Message;
+            }
+            catch (SqlException sqlException)
+            {
+                ViewBag.SqlExceptionMessage = sqlException.Message;
+            }
+            catch (Exception genericException)
+            {
+                ViewBag.ExceptionMessage = genericException.Message;
+            }
+
+            return View("~/Views/Errors/Error.cshtml");
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int? id)
+        {
+            try
+            {
+                if (id == 0)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                Patients patient = db.Patients.SingleOrDefault(p => p.Patient_Id == id);
+
+                if (patient == null)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                return View(patient);
+            }
+            catch (SqlException sqlException)
+            {
+                ViewBag.SqlExceptionMessage = sqlException.Message;
+            }
+            catch (Exception genericException)
+            {
+                ViewBag.ExceptionMessage = genericException.Message;
+            }
+
+            return View("~/Views/Errors/Error.cshtml");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(FormCollection form)
+        {
+            try
+            {
+                int id = Convert.ToInt32(form["patient_id"]);
+                Patients patient = db.Patients.Find(id);
+                db.Patients.Remove(patient);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch (DbUpdateException dbException)
+            {
+                ViewBag.DbExceptionMessage = dbException.Message;
+            }
+            catch (SqlException sqlException)
+            {
+                ViewBag.SqlExceptionMessage = sqlException.Message;
+            }
+            catch (Exception genericException)
+            {
+                ViewBag.ExceptionMessage = genericException.Message;
+            }
+
+            return View("~/Views/Errors/Error.cshtml");
         }
     }
 }
