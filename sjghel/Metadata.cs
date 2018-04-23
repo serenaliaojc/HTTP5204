d217318bfc650a68dda2ec886b822a0708a4d550
serenaliaojc <36046972+serenaliaojc@users.noreply.github.com>
2018-04-23 14:20:08 -0400

    sjghel files

---
+++ sjghel/Metadata.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sjghel_Project.Models
+{
+    public class NewsMetadata
+    {
+        [Key]
+        public int news_id { get; set; }
+
+        [Display(Name = "Post Time")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:G}", ApplyFormatInEditMode = true)]
+        public System.DateTime news_post_time { get; set; }
+
+        [Display(Name = "Last Edit")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:G}", ApplyFormatInEditMode = true)]
+        public System.DateTime news_edit_time { get; set; }
+
+        [Display(Name = "News Title")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please enter the title.")]
+        [StringLength(100, ErrorMessage = "No more than 100 characters.")]
+        public string news_title { get; set; }
+
+        [Display(Name = "News Brief")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please enter the brief.")]
+        [StringLength(255, ErrorMessage = "No more than 255 characters.")]
+        public string news_brief { get; set; }
+
+        [Display(Name = "News Content")]
+        [DataType(DataType.MultilineText)]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please enter the content.")]
+        public string news_content { get; set; }
+
+        [Display(Name = "News Picture")]
+        public string news_pic_path { get; set; }
+    }
+
+    public class AdminMetadata
+    {
+        [Key]
+        [Display(Name = "Admin ID")]
+        public int admin_id { get; set; }
+
+        [Display(Name = "Administrator")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please enter the name.")]
+        [StringLength(50, ErrorMessage = "No more than 50 characters.")]
+        public string admin_name { get; set; }
+
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        [StringLength(16, MinimumLength = 8, ErrorMessage = "Password needs to be 8-16 characters long.")]
+        public string admin_password { get; set; }
+
+        [Display(Name = "Admin Role")]
+        public string admin_role { get; set; }
+    }
+
+}
---
+++ sjghel/NewsController.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Sjghel_Project.Models;
+using System.Data.Entity.Infrastructure;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace Sjghel_Project.Controllers
+{
+    public class NewsController : Controller
+    {
+        private HospitalEntities db = new HospitalEntities();
+
+        // GET: News
+        public ActionResult Index()
+        {
+            try
+            {
+                return View(db.News.ToList());
+            }
+            catch(Exception exception)
+            {
+                ViewBag.ExceptionMessage = exception.Message;
+            }
+
+            return View("~/Views/Shared/Error.cshtml");
+        }
+
+        [HttpPost]
+        public PartialViewResult Search(FormCollection form)
+        {
+            string search_term = form["term"];
+            List<News> news = new List<News>();
+
+            if (!String.IsNullOrWhiteSpace(search_term))
+            {
+                try
+                {
+                    news = db.News.Where(n => n.news_title.ToUpper().Contains(search_term.ToUpper())).ToList();
+                }
+                catch(Exception exception)
+                {
+                    ViewBag.ExceptionMessage = exception.Message;
+                    return PartialView("~/Views/Shared/_Partial_Error.cshtml");
+                }
+            }
+
+            return PartialView("~/Views/News/_Search_Result.cshtml", news);
+        }
+
+        [Authorize(Roles = "grand_admin, news_editor")]
+        public ActionResult Admin()
+        {
+            try
+            {
+                return View(db.News.ToList());
+            }
+            catch (Exception exception)
+            {
+                ViewBag.ExceptionMessage = exception.Message;
+            }
+
+            return View("~/Views/Shared/Error.cshtml");
+        }
+
+        // GET: News/Details/5
+        public ActionResult Details(int? id)
+        {
+            try
+            {
+                if (id == null)
+                {
+                    return RedirectToAction("Index");
+                }
+                News news = db.News.Find(id);
+                news.news_content = news.news_content.Replace(Environment.NewLine, "<br />");
+                if (news == null)
+                {
+                    //return HttpNotFound();
+                    ViewBag.ExceptionMessage = "Sorry, cannot find the article.";
+                    return View("~/Views/Shared/Error.cshtml");
+                }
+                return View(news);
+            }
+            catch (Exception exception)
+            {
+                ViewBag.ExceptionMessage = exception.Message;
+            }
+
+            return View("~/Views/Shared/Error.cshtml");
+        }
+
+        // GET: News/Create
+        [Authorize(Roles = "grand_admin, news_editor")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: News/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "grand_admin, news_editor")]
+        public ActionResult Create([Bind(Include = "news_id,news_post_time,news_edit_time,news_title,news_brief,news_content")] News news, HttpPostedFileBase news_pic)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    string pic_default = "default.jpg";
+                    news.news_pic_path = pic_default;
+                    if (news_pic != null )
+                    {
+                        string imgName = Path.GetFileName(news_pic.FileName);
+                        string filePathOnServer = Path.Combine(Server.MapPath("~/Images/News/"), imgName);
+                        news_pic.SaveAs(filePathOnServer);
+                        news.news_pic_path = imgName;
+                    }
+                    news.news_post_time = DateTime.Now;
+                    news.news_edit_time = DateTime.Now;
+                    db.News.Add(news);
+                    db.SaveChanges();
+                    return RedirectToAction("Details", new { id = news.news_id });
+                }
+
+                return View(news);
+            }
+            catch(Exception exception)
+            {
+                ViewBag.ExceptionMessage = exception.Message;
+            }
+
+            return View("~/Views/Shared/Error.cshtml");
+        }
+
+        // GET: News/Edit/5
+        [Authorize(Roles = "grand_admin, news_editor")]
+        public ActionResult Edit(int? id)
+        {
+            try
+            {
+                if (id == null)
+                {
+                    return RedirectToAction("Admin");
+                }
+                News news = db.News.Find(id);
+                if (news == null)
+                {
+                    ViewBag.ExceptionMessage = "Sorry, cannot find the article.";
+                    return View("~/Views/Shared/Error.cshtml");
+                }
+                //TempData["NewsPic"] = news.news_pic_path;
+                return View(news);
+            }
+            catch (Exception exception)
+            {
+                ViewBag.ExceptionMessage = exception.Message;
+            }
+
+            return View("~/Views/Shared/Error.cshtml");
+        }
+
+        // POST: News/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "grand_admin, news_editor")]
+        public ActionResult Edit([Bind(Include = "news_id,news_post_time,news_edit_time,news_title,news_brief,news_content")] News news, HttpPostedFileBase news_pic)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    if (news_pic != null)
+                    {
+                        string imgName = Path.GetFileName(news_pic.FileName);
+                        string filePathOnServer = Path.Combine(Server.MapPath("~/Images/News/"), imgName);
+
+                        //string pathToRemoveFile = Request.MapPath("~/Images/News/" + TempData["NewsPic"]);
+                        //string pathToRemoveFile = Path.Combine(Server.MapPath("~/Images/News/" + TempData["NewsPic"]));
+                        //System.IO.File.Delete(pathToRemoveFile);
+                        if (news.news_pic_path != "default.jpg")
+                        {
+                            string pathToRemoveFile = Path.Combine(Server.MapPath("~/Images/News/"), news.news_pic_path);
+                            System.IO.File.Delete(pathToRemoveFile);
+                        }
+
+                        news_pic.SaveAs(filePathOnServer);
+                        news.news_pic_path = imgName;
+                    }
+                    news.news_edit_time = DateTime.Now;
+                    db.Entry(news).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("Details", new { id = news.news_id });
+                }
+                return View(news);
+            }
+            catch (DbUpdateException DbException)
+            {
+                ViewBag.DbExceptionMessage = ErrorHandler.DbUpdateHandler(DbException);
+            }
+            catch (SqlException sqlException)
+            {
+                ViewBag.SqlExceptionMessage = sqlException.Message;
+            }
+            catch (Exception exception)
+            {
+                ViewBag.ExceptionMessage = exception.Message;
+            }
+
+            return View("~/Views/Shared/Error.cshtml");
+        }
+
+        // GET: News/Delete/5
+        [Authorize(Roles = "grand_admin, news_editor")]
+        public ActionResult Delete(int? id)
+        {
+            try
+            {
+                if (id == null)
+                {
+                    return RedirectToAction("Admin");
+                }
+                News news = db.News.Find(id);
+                if (news == null)
+                {
+                    ViewBag.ExceptionMessage = "Sorry, cannot delete the article.";
+                    return View("~/Views/Shared/Error.cshtml");
+                }
+                return View(news);
+            }
+            catch (DbUpdateException DbException)
+            {
+                ViewBag.DbExceptionMessage = ErrorHandler.DbUpdateHandler(DbException);
+            }
+            catch (Exception exception)
+            {
+                ViewBag.ExceptionMessage = exception.Message;
+            }
+
+            return View("~/Views/Shared/Error.cshtml");
+        }
+
+        // POST: News/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "grand_admin, news_editor")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            try
+            {
+                News news = db.News.Find(id);
+                if (news.news_pic_path != "default.jpg")
+                {
+                    string pathToRemoveFile = Path.Combine(Server.MapPath("~/Images/News/"), news.news_pic_path);
+                    System.IO.File.Delete(pathToRemoveFile);
+                }
+                db.News.Remove(news);
+                db.SaveChanges();
+                return RedirectToAction("Admin");
+            }
+            catch (DbUpdateException DbException)
+            {
+                //ViewBag only works when returning a view, not redirecting to actions. TempData can persist across Actions
+                TempData["DbExceptionMessage"] = "Cannot delete: " + ErrorHandler.DbUpdateHandler(DbException);
+            }
+            catch (SqlException sqlException)
+            {
+                TempData["SqlExceptionMessage"] = sqlException.Message;
+            }
+            catch (Exception genericException)
+            {
+                TempData["ExceptionMessage"] = genericException.Message;
+            }
+
+            return View("~/Views/Shared/Error.cshtml");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
